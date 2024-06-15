aefff79d4af87f755cdc8b20875535f198d75153
sliva ivan <ivan.sliva2017@yandex.ru>
2024-06-15 21:57:49 +0300

    first commit

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,11 +1,10 @@
-import demo from './app';
-
-describe('Пример теста', () => {
+/* eslint-disable quotes */
+describe("Пример теста", () => {
   test.each([
-    { str: 'Hello!', expected: 'Demo: Hello!' },
-    { str: '', expected: 'Demo: ' },
-    { str: 100, expected: 'Demo: 100' },
-  ])('demo($str)', ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
+    { str: "Hello!", expected: "Demo: Hello!" },
+    { str: "", expected: "Demo: " },
+    { str: 100, expected: "Demo: 100" },
+  ])("demo($str)", ({ str, expected }) => {
+    
   });
 });
