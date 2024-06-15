af11b7f542e2296ea1811e4ed42ae7e2da99ae72
sliva ivan <ivan.sliva2017@yandex.ru>
2024-06-15 17:29:22 +0300

    first commit

--- src/js/goblin.js
+++ src/js/goblin.js
@@ -1,16 +1,18 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line import/prefer-default-export
 export function onGoblinRandom() {
-    const position = [...document.querySelectorAll(".block")];
-    const random = Math.floor(Math.random() * position.length);
-    position[random].classList.add("goblin");
-    setInterval(changePosition, 1000, position);
+  const position = [...document.querySelectorAll('.block')];
+  const random = Math.floor(Math.random() * position.length);
+  position[random].classList.add('goblin');
+  setInterval(changePosition, 1000, position);
+}
+function changePosition() {
+  const position = [...document.querySelectorAll('.block')];
+  const goblin = document.querySelector('.goblin');
+  const posGoblin = position.indexOf(goblin);
+  const random = Math.floor(Math.random() * position.length);
+  if (posGoblin !== random) {
+    position[posGoblin].classList.remove('goblin');
+    position[random].classList.add('goblin');
   }
-  function changePosition() {
-    const position = [...document.querySelectorAll(".block")];
-    const goblin = document.querySelector(".goblin");
-    const posGoblin = position.indexOf(goblin);
-    const random = Math.floor(Math.random() * position.length);
-    if (posGoblin !== random) {
-      position[posGoblin].classList.remove("goblin");
-      position[random].classList.add("goblin");
-    }
-  }
+}
